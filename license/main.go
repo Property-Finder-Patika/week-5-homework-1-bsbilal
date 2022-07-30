9fd2ee75a6a91e63973097195d150907d5bf8ce4
Basulas Bilal <bilal530@gmaill.com>
2022-07-30 21:10:29 +0300

    func checkpoint

--- license/main.go
+++ license/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
@@ -9,18 +10,19 @@ func main() {
 }
 
 type ILicense interface {
-	UseKey(cmd string) (resp string, err error)
+	UseKey() (resp string, err error)
 }
 
 type Product struct {
+	id              string
 	name            string
 	validLicenseKey string
 	currentCapacity int
 	maxCapacity     int
 }
 
-func (p *Product) UseKey() {
-
+func (p *Product) UseKey() (string, error) {
+	return "License key used.", nil
 }
 
 type LicenseKey struct {
@@ -29,9 +31,20 @@ type LicenseKey struct {
 
 type ProductProxy struct {
 	product Product
-	key     LicenseKey
+	key     *LicenseKey
 }
 
-func (p *ProductProxy) UseKey() {
+func NewKeyProxy(k *LicenseKey, p *Product) *ProductProxy {
+	return &ProductProxy{*p, k}
+}
 
+func (p *ProductProxy) UseKey() (string, error) {
+	if p.product.validLicenseKey != p.key.key {
+		return "Error", errors.New("This key does not work for this product.")
+
+	} else if p.product.currentCapacity+1 > p.product.currentCapacity {
+		return "Error", errors.New("The capacity is full for this product.")
+	} else {
+		return "OK", nil
+	}
 }
